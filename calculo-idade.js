7d2c9c53122d608ea3c0630582254b24bd64e947
Vinícius <viniciustrindadeworks@gmail.com>
2024-01-18 08:07:13 -0300

    Criação de uma função prompt.

---
+++ calculo-idade.js
@@ -0,0 +1,12 @@
+        var anoNascimento = prompt("Insira o ano de seu nascimento");
+
+        while(anoNascimento >= 2024){
+            alert('Por favor, insira seu ano de nascimento corretamente.')
+            anoNascimento = prompt('Insira o ano de seu nascimento')
+        }
+
+        var anoAtual = prompt("Insira o ano atual");
+
+        var idadeFinal = anoAtual - anoNascimento
+
+        alert('A idade final é: '+ idadeFinal);
